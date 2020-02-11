782c46740319cd60de880bb1a19d4335af169d34
aakashDeveloper <ahanda205@gmail.com>
2020-02-11 02:34:43 +0000

    a

--- loginapp/src/app/app-routing.module.ts
+++ loginapp/src/app/app-routing.module.ts
@@ -3,19 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegisterFormComponent } from './register-form/registerForm.component';
 import { LoginComponent } from './login-form/loginForm.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ProfileGaurdService } from './profile-gaurd.service';
 
 
 const routes: Routes = [
-  {path:'register', component: RegisterFormComponent},
-  {path:'login', component: LoginComponent},
-  {path:'profile', component:ProfileComponent},
-  {path:'', redirectTo:'register', pathMatch:'full'}
-]
+  {path: 'register', component: RegisterFormComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', canActivate: [ProfileGaurdService], component: ProfileComponent},
+  {path: '', redirectTo: 'register', pathMatch: 'full'}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers:[]
+  providers: [
+    ProfileGaurdService
+  ]
 })
 
 export class AppRoutingModule {
